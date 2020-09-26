841213fcb9673934304fb2d7eaeb2f8e911d4d65
Tyler Webb <tgwebb@gus.pittstate.edu>
2020-09-25 21:49:11 -0500

    fixed route and JSON response

--- starter/routes/weather.js
+++ starter/routes/weather.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-let weather = {
-    temp: 90.5,
-    unit: "Fahrenheit",
-    cloudy: false,
-    precipitationChance: 0
-}
 router.get('/', function(req, res, next) {
 
-  res.render('weather', { title: 'Express' }, {temp: '90.5'}, {unit: 'Fahrenheit'},
-  {cloudy: false, precipitationChance: 0});
+  res.send({temp: '90.5', unit: 'Fahrenheit',
+  cloudy: false, precipitationChance: 0});
 });
 
 module.exports = router;
